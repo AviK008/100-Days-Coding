024a969cef2e35f2ad5658248cd825ba09912807
AviK008 <aviral.32416@stu.upes.ac.in>
2026-08-28 00:25:38 +0530

    Day 19 Solutions

---
+++ Day 19/Question_38.c
@@ -0,0 +1,25 @@
+/*
+Write a program to find the sum of digits of a number.
+Date: 28/08/2026
+*/
+
+#include <stdio.h>
+int main(){
+    int num, sum = 0, remainder;
+
+    printf("Enter The Number: ");
+    scanf("%d", &num);
+
+    if(num < 0){                
+        num = -num;
+    }
+
+    while(num > 0){
+        remainder = num % 10;   
+        sum += remainder;       
+        num /= 10;              
+    }
+
+    printf("%d\n", sum);
+    return 0;
+}
