abb728c5edaa0abf66f9230d0086236a350d1824
AviK008 <aviral.32416@stu.upes.ac.in>
2026-08-29 18:34:29 +0530

    Day 20 Solutions

---
+++ Day 20/Question_39.c
@@ -0,0 +1,25 @@
+/*
+Write a program to find the product of odd digits of a number.
+Date: 29/08/2026
+*/
+
+#include <stdio.h>
+int main(){
+    long long n, digit;
+    long long product = 1;
+
+    printf("Enter A Number: ");
+    scanf("%lld", &n);
+
+    while(n > 0){
+        digit = n % 10;
+
+        if(digit % 2 != 0){
+            product = product * digit;
+        }
+        n = n / 10;
+    }
+
+    printf("%lld", product);
+    return 0;
+}
