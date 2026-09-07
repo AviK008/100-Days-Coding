b985c3d0773bc20160fd9197e057ba54ccea2de4
AviK008 <aviral.32416@stu.upes.ac.in>
2026-09-07 18:44:28 +0530

    Day 29 Solutions

---
+++ Day 29/Question_57.c
@@ -0,0 +1,23 @@
+/*
+Find the sum of array elements.
+Date: 07/09/2026
+*/
+
+#include <stdio.h>
+int main(){
+    int n, i, sum = 0;
+
+    printf("Enter The Number Of Elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for(i = 0; i < n; i++){
+        printf("Enter Element %d: ", i + 1);
+        scanf("%d", &arr[i]);
+        sum += arr[i];
+    }
+
+    printf("%d", sum);
+    return 0;
+}
