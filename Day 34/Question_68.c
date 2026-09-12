61b1343b0726433b26301250e00fbc07f4c12347
AviK008 <aviral.32416@stu.upes.ac.in>
2026-09-12 08:18:24 +0530

    Day 34 Solutions

---
+++ Day 34/Question_68.c
@@ -0,0 +1,31 @@
+/*
+Delete an element from an array.
+Date: 12/09/2026
+*/
+
+#include <stdio.h>
+int main(){
+    int n, arr[100], pos;
+    
+    printf("Enter The Number Of Elements In The Array: ");
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++){
+        printf("Enter Element %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter The Position Of The Element To Be Deleted: ");
+    scanf("%d", &pos);
+
+    for(int i = pos; i < n - 1; i++){
+        arr[i] = arr[i + 1];
+    }
+
+    n--;
+
+    for(int i = 0; i < n; i++){
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
