de04eb2c7b160ead17f86be70ab3981c8ebb4622
AviK008 <aviral.32416@stu.upes.ac.in>
2026-09-24 20:50:07 +0530

    Day 46 Solutions

---
+++ Day 46/Question_92.c
@@ -0,0 +1,24 @@
+/*
+Find the first repeating lowercase alphabet in a string.
+Date:24/09/2026
+*/
+
+#include <stdio.h>
+int main(){
+    char str[100];
+    int count[26] = {0};
+    int i;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    for(i = 0; str[i] != '\0'; i++){
+        count[str[i] - 'a']++;
+
+        if(count[str[i] - 'a'] == 2){
+            printf("%c", str[i]);
+            break;
+        }
+    }
+    return 0;
+}
